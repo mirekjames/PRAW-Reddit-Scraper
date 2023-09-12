61859189182403cb92390d743ac820753135d748
mirekjames <37596280+mirekjames@users.noreply.github.com>
2023-09-12 17:17:10 -0400

    Sort, Awards args
    Added new argument 'sort' which allows you to find posts by top, new, rising, or hot. New argument 'awards' also allows you to toggle whether or not you want to collect data on awards.

--- scrape.py
+++ scrape.py
@@ -9,13 +9,16 @@ from dateutil.tz import tzutc, tzlocal
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-subreddit", help="Please enter the name of the subreddit you want to scrape.", default="all")
-parser.add_argument("-time", help="please enter a time period to search: all, day, hour, month, week, year. default is all", default="all")
-parser.add_argument("-numberOfPosts", help="please enter a number of posts to scrape", default="1000")
-parser.add_argument("-query", help="please enter a query to search for in the subreddit", required=False)
-parser.add_argument("-filename", help="enter a filename")
+parser.add_argument("-sort", help="Which algorithm is used to determine the scraped posts. Options are 'top' 'new' 'rising' 'hot'. Default is top.", default="top")
+parser.add_argument("-time", help="If you're sorting by top, please enter a time period to search: all, day, hour, month, week, year. default is all", default="all")
+parser.add_argument("-numberOfPosts", help="Please enter a number of posts to scrape. Default is 1000.", default="5")
+parser.add_argument("-query", help="Please enter a query to search for in the subreddit.", required=False)
+parser.add_argument("-filename", help="Please enter a filename for the scraped data.")
+parser.add_argument("-awards", help="If you want to get the awards a post / comment has (e.g. Reddit Gold), set -awards to y. Warning: the awards on some subreddits may mess up your data.", default="no")
 args = parser.parse_args()
 
-def utcToEST(utcFloat):
+# Converts scraped data time stamps from UTC to 
+def utcToLocal(utcFloat):
     utcTime = datetime.fromtimestamp(utcFloat)
     convertedTime = utcTime.astimezone(tzlocal())
     return convertedTime
@@ -52,7 +55,14 @@ def getPosts():
         posts = subreddit.search(args.query, limit=int(args.numberOfPosts))
         print("scraping: " + args.query) 
     else:
-        posts = subreddit.top(time_filter=args.time, limit=int(args.numberOfPosts))
+        if (args.sort == "top"):
+            posts = subreddit.top(time_filter=args.time, limit=int(args.numberOfPosts))
+        elif (args.sort == "new"):
+            posts = subreddit.new(limit=int(args.numberOfPosts))
+        elif (args.sort == "rising"):
+            posts = subreddit.rising(limit=int(args.numberOfPosts))
+        elif (args.sort == "hot"):
+            posts = subreddit.hot(limit=int(args.numberOfPosts))
 
     posts_dict = {"ID": [], "Subreddit": [], "Date": [], "Title": [], "Post Text": [], "Post Flair": [], 
                     "Author": [], "Author Flair": [], "Post Flair": [], "Score": [], "Upvote Ratio": [],
@@ -67,7 +77,7 @@ def getPosts():
         posts_dict["Subreddit"].append(post.subreddit)
 
         # Time of Post
-        posts_dict["Date"].append(utcToEST(post.created_utc))
+        posts_dict["Date"].append(utcToLocal(post.created_utc))
 
         # Title of each post
         posts_dict["Title"].append(post.title)
@@ -106,15 +116,20 @@ def getPosts():
         posts_dict["NSFW"].append(post.over_18)
 
         # Awards
-        if post.all_awardings:
-            awardsString = []
-            awardsString = '"' + ''.join(map(str,post.all_awardings)) + '"'
-            posts_dict["Awards"].append(awardsString)
-            posts_dict["Awarded"].append("TRUE")
+        if args.awards == True:  
+            if post.all_awardings:
+                awardsString = []
+                awardsString = '"' + ''.join(map(str,post.all_awardings)) + '"'
+                posts_dict["Awards"].append(awardsString)
+                posts_dict["Awarded"].append("TRUE")
+            else:
+                posts_dict["Awards"].append(post.all_awardings)
+                posts_dict["Awarded"].append("FALSE")
         else:
-            posts_dict["Awards"].append(post.all_awardings)
-            posts_dict["Awarded"].append("FALSE")
+            posts_dict["Awards"].append("")
+            posts_dict["Awarded"].append("")
 
+    print(posts_dict)
     posts_df = pd.DataFrame(posts_dict)
     posts_df.to_csv("./ScrapedData/" + args.subreddit + "_" + str(args.filename) + "_posts.csv", index=True)
 
@@ -122,8 +137,16 @@ def getUnorderedComments():
     posts = []
     if(args.query is not None):
         posts = subreddit.search(args.query, limit=int(args.numberOfPosts))
+        print("scraping: " + args.query) 
     else:
-        posts = subreddit.top(time_filter=args.time, limit=int(args.numberOfPosts))
+        if (args.sort == "top"):
+            posts = subreddit.top(time_filter=args.time, limit=int(args.numberOfPosts))
+        elif (args.sort == "new"):
+            posts = subreddit.new(limit=int(args.numberOfPosts))
+        elif (args.sort == "rising"):
+            posts = subreddit.rising(limit=int(args.numberOfPosts))
+        elif (args.sort == "hot"):
+            posts = subreddit.hot(limit=int(args.numberOfPosts))
     comment_submissions = []    
     comment_ids = []
     comment_dates = []
@@ -142,7 +165,7 @@ def getUnorderedComments():
         for comment in submission.comments.list():
             comment_submissions.append(comment.submission.id)
             comment_ids.append(comment.id)
-            comment_dates.append(utcToEST(comment.created_utc))
+            comment_dates.append(utcToLocal(comment.created_utc))
             comment_authors.append((str(comment.author)))
             comment_authorflairs.append(comment.author_flair_text)
             comment_bodies.append(comment.body)
@@ -151,28 +174,19 @@ def getUnorderedComments():
             comment_permalinks.append(comment.permalink)
             comment_postlocked.append(submission.locked)
             comment_NSFW.append(submission.over_18)
-            if(comment.all_awardings):
-                awardsString = []
-                awardsString = '"' + ''.join(map(str,comment.all_awardings)) + '"'
-                comment_awards.append(awardsString)
-                comment_awarded.append("TRUE")
+            if args.awards == True:  
+                if(comment.all_awardings):
+                    awardsString = []
+                    awardsString = '"' + ''.join(map(str,comment.all_awardings)) + '"'
+                    comment_awards.append(awardsString)
+                    comment_awarded.append("TRUE")
+                else:
+                    comment_awards.append(comment.all_awardings)
+                    comment_awarded.append("FALSE")
             else:
-                comment_awards.append(comment.all_awardings)
-                comment_awarded.append("FALSE")
-            # print("Submissions: " + len(comment_submissions))
-            # print("IDs: " + len(comment_ids))      
-            # print("Dates: " + len(comment_dates))   
-            # print("Authors: " + len(comment_authors))
-            # print("Author Flairs: " + len(comment_authorflairs))   
-            # comment_authorflairs = []
-            # comment_bodies = []
-            # comment_scores = []
-            # comment_replies = []
-            # comment_permalinks = []
-            # comment_postlocked = []
-            # comment_NSFW = []
-            # comment_awards = []
-            # comment_awarded = []
+                comment_awards.append("")
+                comment_awarded.append("")
+
     comment_dict = {"Submission ID": comment_submissions, "Comment ID": comment_ids, "Comment Date": comment_dates, "Author": comment_authors, "AuthorFlair": comment_authorflairs, "Text": comment_bodies, "Score": comment_scores, "Replies": comment_replies, "Link": comment_permalinks, "Post Locked": comment_postlocked, "Post NSFW": comment_NSFW, "Awards": comment_awards,  "Awarded": comment_awarded}       
     comments_df = pd.DataFrame(comment_dict)
     comments_df.to_csv("./ScrapedData/" + args.subreddit + "_" + str(args.filename) + "_comments.csv", index=True)
@@ -181,17 +195,29 @@ def getOrderedComments():
     posts = []
     if(args.query is not None):
         posts = subreddit.search(args.query, limit=int(args.numberOfPosts))
+        print("scraping: " + args.query) 
     else:
-        posts = subreddit.top(time_filter=args.time, limit=int(args.numberOfPosts))
+        if (args.sort == "top"):
+            posts = subreddit.top(time_filter=args.time, limit=int(args.numberOfPosts))
+        elif (args.sort == "new"):
+            posts = subreddit.new(limit=int(args.numberOfPosts))
+        elif (args.sort == "rising"):
+            posts = subreddit.rising(limit=int(args.numberOfPosts))
+        elif (args.sort == "hot"):
+            posts = subreddit.hot(limit=int(args.numberOfPosts))
     for submission in posts:
         awarded = ""
-        if submission.all_awardings:
-            awarded = "TRUE"
+        if args.awards == True:  
+            if submission.all_awardings:
+                awarded = "TRUE"
+            else:
+                awarded = "FALSE"
         else:
-            awarded = "FALSE"
+            awarded = ""
+
         with open("./ScrapedData/" + args.subreddit + "_" + str(args.filename) + "_orderedComments.txt", "a") as txt:
             txt.write("Post Data: [" + 'Subreddit: {}, Date: {}, Title: {}, Author: {}, AuthorFlair: {}, Score: {}, ID: {}, Link: {}, Locked: {}, NSFW: {}, Awarded: {}, Comments: {}'.format(str(submission.subreddit).encode("utf-8"),
-                                                                                  str(utcToEST(submission.created_utc)).encode("utf-8"),
+                                                                                  str(utcToLocal(submission.created_utc)).encode("utf-8"),
                                                                                   str(submission.title).encode("utf-8"),
                                                                                   str(submission.author).encode("utf-8"),
                                                                                   str(submission.author_flair_text).encode("utf-8"),
